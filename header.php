eb021db0803d998ee24f2e9930839ef5fa1d59cc
sergio7776 <sergioblancoor@gmail.com>
2021-05-26 21:43:32 -0300

    new

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1 <?php echo ($pg == "inicio")? "active" : ""?>" href="index.php">Inicio</a>
+                            <a class="nav-link px-4 py-1 <?php echo ($pg == "inicio")? "active" : "" ?>" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link px-4 py-1 <?php echo ($pg == "sobre_mi")? "active" : "" ?>" href="sobre-mi.php">Sobre mí</a>
